cc1b393b0377b4095e3140896f0ba0328861d9f4
pepmeg <@>
2024-11-15 17:46:33 +0300

    nnn

--- app/src/main/java/com/example/counterapp/MainActivity.kt
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -8,18 +8,27 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
     private var counter = 0
     private lateinit var counterText: TextView
-    private lateinit var button: Button
+    private lateinit var incrementButton: Button
+    private lateinit var resetButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         counterText = findViewById(R.id.counterText)
-        button = findViewById(R.id.button)
-        button.setOnClickListener {
+        incrementButton = findViewById(R.id.incrementButton)
+        resetButton = findViewById(R.id.resetButton)
+        incrementButton.setOnClickListener {
             counter++
             counterText.text = counter.toString()
-            Log.i("CounterApp", "Кнопка нажата. Текущее значение счетчика: $counter")
+            Log.i("CounterApp", "Кнопка увеличения нажата. Текущее значение счетчика: $counter")
+        }
+        resetButton.setOnClickListener {
+            counter = 0
+            counterText.text = counter.toString()
+            Log.i("CounterApp", "Кнопка сброса нажата. Счетчик сброшен на 0")
         }
     }
 }
