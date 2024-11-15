20ce1276ab31adcd920a926d91d5a2a95414e1bd
pepmeg <@>
2024-11-15 17:21:51 +0300

    nnn

---
+++ app/src/main/java/com/example/counterapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.counterapp
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private var counter = 0
+    private lateinit var counterText: TextView
+    private lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        counterText = findViewById(R.id.counterText)
+        button = findViewById(R.id.button)
+        button.setOnClickListener {
+            counter++
+            counterText.text = counter.toString()
+            Log.i("CounterApp", "Кнопка нажата. Текущее значение счетчика: $counter")
+        }
+    }
+}
